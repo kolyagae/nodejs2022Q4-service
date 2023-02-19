ff1ca6cce28afb52a14b43266fd68893ed00892b
EVGEN <k.e.l@tut.by>
2023-02-19 17:06:26 +0300

    feat: add entity for track

---
+++ src/tracks/track.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class TrackEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  artistId: string | null;
+
+  @Column({ type: 'uuid', nullable: true })
+  albumId: string | null;
+
+  @Column()
+  duration: number;
+}
